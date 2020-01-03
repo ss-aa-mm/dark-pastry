146be76e4124acb42c35c1d7f02f02304305de9d
Giulia <giuliaboggiali@gmail.com>
2020-01-03 15:24:01 +0100

    Implemented enemy movement

---
+++ Dark Pastry/Assets/Scripts/Enemies/Milk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+    public class Milk : NewEnemy
+    {
+        private int _frames;
+        private int _xAxis;
+        private int _yAxis;
+
+        protected override void AssignReferences()
+        {
+            Health = 1f;
+            DamageInflicted = 0.5f;
+            MovementTime = 0.5f;
+            Unit = 0.25f;
+            ItemDropped = Resources.Load<GameObject>("Prefabs/DroppedEgg");
+        }
+
+        protected override void MovementPattern()
+        {
+            MoveRandomly();
+        }
+    }
+}
